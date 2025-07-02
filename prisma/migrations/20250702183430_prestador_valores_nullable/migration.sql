be52652ef24aee693c8ba2fce3f59e36f1b4c3be
Yago Gouvea <yago.gpuvea@gmail.com>
2025-07-02 15:34:53 -0300

    Fix API URL configuration in FotosPopup component

---
+++ prisma/migrations/20250702183430_prestador_valores_nullable/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Prestador" ALTER COLUMN "valor_acionamento" DROP DEFAULT,
+ALTER COLUMN "franquia_horas" DROP DEFAULT,
+ALTER COLUMN "franquia_km" DROP DEFAULT,
+ALTER COLUMN "valor_hora_adc" DROP DEFAULT,
+ALTER COLUMN "valor_km_adc" DROP DEFAULT;
