d1f7f15f8ef8567eb5478228336c7d7b8982d2fc
Yago Gouvea <yago.gpuvea@gmail.com>
2025-07-07 10:09:29 -0300

    fix: corrige comando start no railway.json para uso de serve

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -3,9 +3,11 @@ import { CorsOptions } from 'cors';
 const allowedOrigins = [
   'https://app.painelsegtrack.com.br',
   'http://localhost:5173',
+  'http://localhost:5174',
   'http://localhost:3000',
   'http://localhost:8080',
   'http://127.0.0.1:5173',
+  'http://127.0.0.1:5174',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:8080'
 ];
