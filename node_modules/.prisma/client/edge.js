78b8615bd3fcb52c01232a3b92413a29edf57720
Yago Gouvea <yago.gpuvea@gmail.com>
2025-07-18 21:33:36 -0300

    Fix Railway build configuration

--- node_modules/.prisma/client/edge.js
+++ node_modules/.prisma/client/edge.js
@@ -421,6 +421,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- node_modules/.prisma/client/index.js
+++ node_modules/.prisma/client/index.js
@@ -422,6 +422,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
